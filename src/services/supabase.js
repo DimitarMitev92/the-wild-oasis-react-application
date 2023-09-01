0715d164624971c52300bf5a8cf9870f994691c4
Dimitar Mitev <d.mitev920123@gmail.com>
2023-09-01 16:06:52 +0300

    sorting and filtering of cabins

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://ipqeagqfasntimkulbpr.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwcWVhZ3FmYXNudGlta3VsYnByIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMyOTg0NzYsImV4cCI6MjAwODg3NDQ3Nn0.eAvQexigNxjSxZOE-v4qwteLNOoF0fjgNo8mpBFJiI8";
+export const supabaseUrl = 'https://ipqeagqfasntimkulbpr.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwcWVhZ3FmYXNudGlta3VsYnByIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMyOTg0NzYsImV4cCI6MjAwODg3NDQ3Nn0.eAvQexigNxjSxZOE-v4qwteLNOoF0fjgNo8mpBFJiI8';
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
